3a27be0e63c3c229a5adcdf491de3db0bb1c5546
Glitch (hello-webpage) <none>
2022-09-23 22:02:03 +0000

    🏋👋 Checkpoint ./index.html:5261187/1186 ./landing.css:5261187/11 ./style.css:5261187/14 ./website.js:5261187/1

--- website.js
+++ website.js
@@ -1,5 +1,5 @@
     var i = 0;
-// var A = 0;
+/ var A = 0;
     var tag = document.getElementById("text");
     var html = document.getElementById("text").innerHTML;
     var attr = tag.setAttribute("data", html);
